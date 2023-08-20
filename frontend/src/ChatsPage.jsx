df75d6294cb682e3a7b70020057a4efe02d66712
SHERAN-RANDIKA <randika-se19034@stu.kln.ac.lk>
2023-08-20 15:00:02 +0530

    update chatsPage

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,8 +1,17 @@
-const ChatsPage = () => {
-  return 
-    <>
-      Chats Page
-    </>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(
+        '97ad0c80-f567-4973-a0cb-cc23eeaa8ebb', 
+        props.user.username, props.user.secret
+    );
+    return (
+    <div style={{ height: "100vh" }}>
+        <MultiChatSocket {...chatProps} />
+        <MultiChatWindow {...chatProps} style={{ height: '100%' }} />
+    </div>
+    )
+
 }
 
 export default ChatsPage;
